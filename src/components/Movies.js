36a1897070dc0cf728255d2ee928c71fe95df75d
Joseph Lurvey <joeylurvey@gmail.com>
2023-08-06 17:03:02 -0400

    complete lab

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,18 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movies.map((movie) => (
+        <div key={movie.title}>
+          <h1>{movie.title}</h1>
+          <h2>Time: {movie.time}</h2>
+          <ul key={movie.title}>{movie.genres.map((genre) => (<li key={genre}>{genre}</li>))}</ul>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Movies;
